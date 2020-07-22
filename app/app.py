8c027cdaa818c1e1377ffea7c0c6c0d811c1481f
kj2gh <ben@benlarry.com>
2020-07-21 23:15:21 -0400

    Fix: Update app.py

--- app/app.py
+++ app/app.py
@@ -5,7 +5,6 @@ from flask import Flask, Response
 from flask import render_template
 
 app = Flask(__name__)
-app = Flask(__name__, template_folder='template')
 
 
 def cities_import() -> List[Dict]:
@@ -26,14 +25,6 @@ def cities_import() -> List[Dict]:
     connection.close()
 
     return result
-
-
-@app.route('/')
-def index():
-    user = {'username': 'Miguel'}
-    cities_data = cities_import()
-
-    return render_template('index.html', title='Home', user=user, cities=cities_data)
 
 
 @app.route('/api/cities')
